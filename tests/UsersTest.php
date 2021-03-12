ffe0f21c882a6157166c2ba6fc3365cfea9bc47d
mrlaowilly <willy.meksavanh@hotmail.fr>
2021-03-12 09:22:42 +0100

    modification des tests

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -4,19 +4,24 @@ namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
 use App\Entity\User;
+use Symfony\Component\Yaml\Yaml;
 
 class UsersTest extends ApiTestCase
 {
+
+    // cette function me permet de creer une function 
     public function testCreateUser(): void
     {
+        // ajouter un utilisateur POST sur l'api user, sans passer par le fichier 
         $response = static::createClient()->request('POST', 'api/users', ['json' => [
             'nom' => 'rif',
             'prenom' => 'rif',
             'age' => 17,
         ]]);
-
+// ici je demande si elle existe / bravo elle etait creer 
         $this->assertResponseStatusCodeSame(201);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        // je verifie que la reponse json, correspond bien a ce que j'ai ecrit 
         $this->assertJsonContains([
             '@context' => '/api/contexts/User',
             '@type' => 'User',
@@ -25,17 +30,54 @@ class UsersTest extends ApiTestCase
             'age' => 17
         ]);
     }
+    
+    // pour observer les valeurs des variables
+    /*public function testYamlUser(): void
+    {
+        $value = Yaml::parseFile('tests/users_tests.yaml');
+        
+    }
+    public function testYamlUsers(): void
+    {
+        $value = Yaml::parseFile('tests/users_tests.yaml');
+        if ($value ["array_tests"]["success_ok"] == 200);
+        dump($value);
+    }*/
+
+    public function testCreateYamlUser(): void
+    {
+        $valuename = Yaml::parseFile('tests/users_tests.yaml');
+        $response = static::createClient()->request('POST', 'api/users', ['json' => [
+            'nom' => $valuename ["array_tests"]["nom"],
+            'prenom' => $valuename ["array_tests"]["prenom"],
+            'age' => $valuename ["array_tests"]["age"],
+        ]]);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/User',
+            '@type' => 'User',
+            'nom' => $valuename ["array_tests"]["nom"],
+            'prenom' => $valuename ["array_tests"]["prenom"],
+            'age' => $valuename ["array_tests"]["age"]
+        ]);
+    }
+    // cette function c'est pour supprimer le USer
     public function testDeleteUser(): void
     {
+        $valuename = Yaml::parseFile('tests/users_tests.yaml');
+        // le http client 
         $client = static::createClient();
-        $iri = $this->findIriBy(User::class, ['nom' => 'rif']);
-
+        // je creer ma varible iri je recuprer l'object nom
+        $iri = $this->findIriBy(User::class, ['nom' => $valuename ["array_tests"]["nom"]]);
+        // request delete ici
         $client->request('DELETE', $iri);
-
+        // ici j'attend une reponse
         $this->assertResponseStatusCodeSame(204);
         $this->assertNull(
-            // Through the container, you can access all your services from the tests, including the ORM, the mailer, remote API clients...
-            static::$container->get('doctrine')->getRepository(User::class)->findOneBy(['nom' => 'rif'])
+            /// A travers le container, vous pouvez accéder à tous vos services depuis les tests, y compris l'ORM, le mailer, les clients API distants...
+            static::$container->get('doctrine')->getRepository(User::class)->findOneBy(['nom' => $valuename ["array_tests"]["nom"]])
         );
-    }   
+    }
 }
